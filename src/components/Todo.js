93a970b0a9c006ada967232c4cbd26a3cf050ca9
Jongwon <jwp7073@gmail.com>
2022-10-18 22:17:33 +0900

    fix: 수정 버튼 동작 버그 수정

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,21 +1,20 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
-const Label = styled.label`
-  display: block;
+const Form = styled.form`
+  display: inline;
 `;
 
-const Todo = ({
-  todo,
-  getTodos,
-  handleInputDescription,
-  handleFinish,
-  handleEdit,
-}) => {
-  const handleInputChecked = (e, id) => {
-    const body = {
-      isChecked: e.target.checked,
-    };
-    fetch(`${process.env.REACT_APP_API_URL}/todos/${id}`, {
+const Todo = ({ todo, getTodos }) => {
+  const [isChecked, setIsChecked] = useState(todo.isChecked);
+  const [isEditing, setIsEditing] = useState(false);
+  const [description, setDescription] = useState(todo.description);
+
+  const handleInputChecked = (e) => {
+    setIsChecked(e.target.checked);
+
+    const body = { isChecked: e.target.checked };
+    fetch(`${process.env.REACT_APP_API_URL}/todos/${todo.id}`, {
       method: 'PATCH',
       headers: {
         'Content-type': 'application/json',
@@ -24,8 +23,31 @@ const Todo = ({
     }).then(getTodos);
   };
 
-  const handleDelete = (id) => {
-    fetch(`${process.env.REACT_APP_API_URL}/todos/${id}`, {
+  const handleInputDescription = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleFinish = (e) => {
+    e.preventDefault();
+    const body = { description };
+    fetch(`${process.env.REACT_APP_API_URL}/todos/${todo.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    }).then(() => {
+      getTodos();
+      setIsEditing(false);
+    });
+  };
+
+  const handleDelete = () => {
+    fetch(`${process.env.REACT_APP_API_URL}/todos/${todo.id}`, {
       method: 'DELETE',
       headers: {
         'Content-type': 'application/json',
@@ -34,39 +56,31 @@ const Todo = ({
   };
 
   return (
-    <Label>
+    <div>
       <input
         type="checkbox"
-        checked={todo.isChecked}
-        onChange={(e) => handleInputChecked(e, todo.id)}
+        checked={isChecked}
+        onChange={(e) => {
+          handleInputChecked(e);
+        }}
       />
-      {todo.isEditing ? (
-        <form
-          onSubmit={(e) => {
-            handleFinish(e, todo.id);
-          }}
-        >
+      {isEditing ? (
+        <Form onSubmit={handleFinish}>
           <input
             type="text"
-            value={todo.description}
-            onChange={(e) => handleInputDescription(e, todo.id)}
+            value={description}
+            onChange={handleInputDescription}
           />
           <button type="submit">완료</button>
-        </form>
+        </Form>
       ) : (
         <>
-          <span>{todo.description}</span>
-          <button
-            onClick={() => {
-              handleEdit(todo.id);
-            }}
-          >
-            수정
-          </button>
+          <span>{description}</span>
+          <button onClick={handleEdit}>수정</button>
         </>
       )}
-      <button onClick={() => handleDelete(todo.id)}>삭제</button>
-    </Label>
+      <button onClick={handleDelete}>삭제</button>
+    </div>
   );
 };
 
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import _ from 'lodash';
 import Todo from './Todo';
 import NewTodo from './NewTodo';
 
@@ -22,7 +21,6 @@ const Todos = () => {
     })
       .then((response) => response.json())
       .then((data) => {
-        data.forEach((todo) => (todo.isEditing = false));
         setTodos(data);
       });
   };
@@ -31,55 +29,10 @@ const Todos = () => {
     getTodos();
   }, []);
 
-  const handleInputDescription = (e, id) => {
-    const todosCopy = _.cloneDeep(todos);
-    const todo = todosCopy.filter((todo) => todo.id === id)[0];
-    const idx = todosCopy.indexOf(todo);
-
-    todo.description = e.target.value;
-
-    todosCopy[idx] = todo;
-
-    setTodos(todosCopy);
-  };
-
-  const handleEdit = (id) => {
-    const todosCopy = _.cloneDeep(todos);
-    const todo = todosCopy.filter((todo) => todo.id === id)[0];
-    const idx = todosCopy.indexOf(todo);
-
-    todo.isEditing = true;
-
-    todosCopy[idx] = todo;
-
-    setTodos(todosCopy);
-  };
-
-  const handleFinish = (e, id) => {
-    e.preventDefault();
-    const body = {
-      description: todos.filter((todo) => todo.id === id)[0].description,
-    };
-    fetch(`${process.env.REACT_APP_API_URL}/todos/${id}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify(body),
-    }).then(getTodos);
-  };
-
   return (
     <Wrapper>
       {todos.map((todo) => (
-        <Todo
-          key={todo.id}
-          todo={todo}
-          getTodos={getTodos}
-          handleInputDescription={handleInputDescription}
-          handleFinish={handleFinish}
-          handleEdit={handleEdit}
-        />
+        <Todo key={todo.id} todo={todo} getTodos={getTodos} />
       ))}
       <NewTodo getTodos={getTodos} />
     </Wrapper>
